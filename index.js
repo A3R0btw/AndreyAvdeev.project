7adacb9053539e4fad4632370e9f05f805f457a5
A3R0btw <166328934+A3R0btw@users.noreply.github.com>
2025-05-18 15:45:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,14 @@ setTimeout(function(){
       $("#loading").css("display","none");
     },800);
 },1450);
+
+function triggerDownload(fileName) {
+  var element = document.createElement('a');
+  element.setAttribute('href', fileName);
+  element.setAttribute('download', fileName);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  // Происходит клик, словно совершил его сам программирующий ниндзя
+  element.click();
+  document.body.removeChild(element);
+}
